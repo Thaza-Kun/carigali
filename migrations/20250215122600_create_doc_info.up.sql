4edbcd64c888c0d1a408c1bed35f17e78e64f9a2
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2025-03-23 12:04:50 +0800

    :gear: FIX: Trigger frequency count

--- migrations/20250215122600_create_doc_info.up.sql
+++ migrations/20250215122600_create_doc_info.up.sql
@@ -4,8 +4,27 @@ CREATE TABLE doc_info (
     term_count integer not null
 );
 
-CREATE TRIGGER update_doc_info AFTER INSERT ON term_info
-BEGIN
-    INSERT INTO doc_info (term_count, document) VALUES (1, new.document)
-    ON CONFLICT (document) DO UPDATE SET term_count = term_count + new.occurence;
+CREATE TRIGGER update_doc_info AFTER INSERT ON term_info BEGIN
+INSERT INTO
+    doc_info (term_count, document)
+VALUES
+    (1, new.document) ON CONFLICT (document) DO
+UPDATE
+SET
+    term_count = term_count + new.occurence;
+
+UPDATE term_info
+SET
+    frequency = (
+        SELECT
+            cast(occurence as real) / cast(term_count as real)
+        FROM
+            term_info
+            JOIN doc_info ON term_info.document = doc_info.document
+        WHERE
+            term_info.document = new.document
+    )
+WHERE
+    term_info.document = new.document;
+
 END;
